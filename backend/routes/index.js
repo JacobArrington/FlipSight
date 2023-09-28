013d855fd4ce0af05adc4c844e2caa81c19957e3
Jacob Arrington <Jacobsarrington@gmail.com>
2023-09-27 23:11:25 -0400

    phase 2 done

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,4 +1,7 @@
-// Add a XSRF-TOKEN cookie
+// backend/routes/index.js
+const express = require('express');
+const router = express.Router();
+
 router.get("/api/csrf/restore", (req, res) => {
   const csrfToken = req.csrfToken();
   res.cookie("XSRF-TOKEN", csrfToken);
@@ -6,3 +9,9 @@ router.get("/api/csrf/restore", (req, res) => {
     'XSRF-Token': csrfToken
   });
 });
+
+const apiRouter = require('./api');
+
+router.use('/api', apiRouter);
+
+module.exports = router;
