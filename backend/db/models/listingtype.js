660046b1be888188bb450a5859db53f2584faf57
Jacob Arrington <Jacobsarrington@gmail.com>
2023-10-21 12:24:33 -0400

    fixed rel issue

--- backend/db/models/listingtype.js
+++ backend/db/models/listingtype.js
@@ -10,7 +10,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      ListingType.hasMany(models.item,{foreignKey})
+      ListingType.hasMany(models.Item,{foreignKey: 'listingTypeId'})
     }
   }
   ListingType.init({
