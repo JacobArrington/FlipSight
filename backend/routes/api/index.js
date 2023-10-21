377701a93bf553d404350a503e5ccf13186ae8ca
Jacob Arrington <Jacobsarrington@gmail.com>
2023-10-21 13:53:14 -0400

    making routes

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -4,12 +4,15 @@ const { User } = require('../../db/models');
 const sessionRouter = require('./session.js');
 const usersRouter = require('./users.js');
 const { restoreUser } = require('../../utils/auth.js');
+const itemcomparison = require('../../db/models/itemcomparison.js');
 
 router.use(restoreUser);
 
 router.use('/session', sessionRouter);
 
 router.use('/users', usersRouter);
+
+router.use('/items', itemsRouter)
 
 
 module.exports = router;
---
+++ backend/routes/api/items.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const {User, Item, ItemCondition, ListingType, sequelize} = require('../../db/models')
+const fetch = require('node-fetch')
+const router = express.Router
+
+
+
+
+module.exports = router 
